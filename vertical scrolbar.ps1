be850caa483bf5091f298e68157f8ecd5812f273
kiberkot <dr.techpro@gmail.com>
2022-05-12 13:27:19 +0300

    Create vertical scrolbar.ps1

---
+++ vertical scrolbar.ps1
@@ -0,0 +1,6 @@
+$TextBox1.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]204,[System.Int32]140))
+$TextBox1.Multiline = $true
+$TextBox1.Name = [System.String]'TextBox1'
+$TextBox1.ScrollBars = [System.Windows.Forms.ScrollBars]::Vertical
+$TextBox1.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]500,[System.Int32]247))
+$TextBox1.TabIndex = [System.Int32]0
