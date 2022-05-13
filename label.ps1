1ff4d10da39dae7a13e2fb9ffbb3cdd505e9ee3a
kiberkot <dr.techpro@gmail.com>
2022-05-13 14:07:49 +0300

    Create label.ps1

---
+++ label.ps1
@@ -0,0 +1,8 @@
+#Label1
+#
+$Label1.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]12,[System.Int32]9))
+$Label1.Name = [System.String]'Label1'
+$Label1.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]100,[System.Int32]23))
+$Label1.TabIndex = [System.Int32]0
+$Label1.Text = [System.String]'Label1'
+$Label1.add_Click($Label1_Click)
